47f2129d374e0d9a12a08959bad9aed19c90b8ba
Alexis Kaufman <alexiskaufman@gmail.com>
2024-01-12 10:50:39 -0600

    add tooltips

--- howrse-plus-plus/components/PopupSettings/PopupSettings.tsx
+++ howrse-plus-plus/components/PopupSettings/PopupSettings.tsx
@@ -1,3 +1,51 @@
-export default function PopupSettings(props) {
-  return <div></div>
+import { useEffect, useState } from "react"
+import { Form, InputGroup, OverlayTrigger, Tooltip } from "react-bootstrap"
+
+import { getData, setData } from "~chrome_store"
+import { type BooleanSettings, booleanSettings } from "~settingsObtainer"
+
+import styles from "./PopupSettings.module.css"
+
+export default function PopupSettings() {
+  const [currentSettings, setCurrentSettings] = useState<
+    Record<keyof BooleanSettings, boolean>
+  >({})
+  useEffect(() => {
+    const newCurrSettings: Record<keyof BooleanSettings, boolean> = {}
+    Object.keys(booleanSettings).map((key) => {
+      getData(key)
+        .then((dat) => {
+          newCurrSettings[key] = dat
+        })
+        .catch((err) => {
+          console.error(`[ERR] ${err} getting data: `, key)
+        })
+    })
+    setCurrentSettings(newCurrSettings)
+  }, [])
+  return (
+    <Form>
+      {Object.keys(booleanSettings)?.map((settingKey, index) => {
+        const tooltip = (
+          <Tooltip>{booleanSettings[settingKey]?.tooltip ?? "err"}</Tooltip>
+        )
+        return (
+          <div className={styles.flexRowSpaceBtwn} key={settingKey}>
+            <Form.Check
+              checked={currentSettings[settingKey] ?? false}
+              onChange={async (e) => {
+                await setData(settingKey, e.target.checked)
+              }}
+              type="switch"
+              id={settingKey}
+              label={booleanSettings[settingKey]?.label ?? "err"}
+            />
+            <OverlayTrigger placement="top" overlay={tooltip}>
+              <div className={styles.infoHoverLbl}>?</div>
+            </OverlayTrigger>
+          </div>
+        )
+      })}
+    </Form>
+  )
 }
--- howrse-plus-plus/settingsObtainer.ts
+++ howrse-plus-plus/settingsObtainer.ts
@@ -6,18 +6,35 @@ export type SpecialtyContext = {
 
 export type CoatRarityCache = Record<string, Record<string, number>>
 
-export type Preset = {
-  horseNameIconsEnabled: boolean
-  extraHorseButtonsEnabled: boolean
-  useCustomHorseName: boolean
-  newFoalSuggestionsEnabled: boolean
+export type BooleanSettings = Record<string, { label: string; tooltip: string }>
+export const booleanSettings: BooleanSettings = {
+  horseNameIconsEnabled: {
+    label: "Add Extra Icons",
+    tooltip:
+      "Adds extra icons to a horse's page to provide more information without having the scroll. Currently, this entails BMIs, coat rarity, purebred status, and an indicator for if a mare is ready to be covered."
+  },
+  extraHorseButtonsEnabled: {
+    label: "Add Extra Icons",
+    tooltip:
+      "Adds extra icons to a horse's page to provide more information without having the scroll. Currently, this entails BMIs, coat rarity, purebred status, and an indicator for if a mare is ready to be covered."
+  },
+  useCustomHorseName: {
+    label: "Add Extra Icons",
+    tooltip:
+      "Adds extra icons to a horse's page to provide more information without having the scroll. Currently, this entails BMIs, coat rarity, purebred status, and an indicator for if a mare is ready to be covered."
+  },
+  newFoalSuggestionsEnabled: {
+    label: "Add Extra Icons",
+    tooltip:
+      "Adds extra icons to a horse's page to provide more information without having the scroll. Currently, this entails BMIs, coat rarity, purebred status, and an indicator for if a mare is ready to be covered."
+  }
 }
 
 export type MiscData = {
   coatRarityCache: CoatRarityCache
 }
 
-export const settingsDefaults: Preset = {
+export const settingsDefaults: Record<keyof BooleanSettings, boolean> = {
   horseNameIconsEnabled: false,
   extraHorseButtonsEnabled: false,
   useCustomHorseName: false,
